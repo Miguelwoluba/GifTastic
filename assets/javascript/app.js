ce021757ea89e79245951a556e5ff7c056e7717c
miguel mejia <mejiamiguel4@gmail.com>
2018-05-07 23:55:55 -0600

    more functunality, got to work on the animated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 
-    var topics = ["motorcycles","dirtbikes","cars","trials motorcycles","motorcross"];
+    var topics = ["motorcycles", "dirtbikes", "cars", "trials motorcycles", "motorcross", "f1", "rally", "downhill bikes", "tricycles", "bicycle"];
     
     for(var i =0; i < topics.length; i++){
         var button = $("<button>");
@@ -10,13 +10,15 @@ $(document).ready(function () {
         $("#buttons").append(button);
         button.append(buttonText);
         button.attr("data-button", topics[i]);
+        button.addClass("singleButton");
 
     }
     
-    $("#buttons").on("click", function () {
+    $(".singleButton").on("click", function () {
+        
         var topicsGifs = $(this).attr("data-button");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topics + "&api_key=qtLhTGYOvWfD24dxX5x988LKgcJz8nOy&limit=10";
+        topicsGifs + "&api_key=qtLhTGYOvWfD24dxX5x988LKgcJz8nOy&limit=10";
 
             $.ajax({
         url: queryURL,
@@ -25,7 +27,7 @@ $(document).ready(function () {
                 .then(function (response) {
                 var results = response.data;
                 console.log(response);
-
+                    $("#gifs-container").empty();
                     for (var i = 0; i < results.length; i++) {
                         var gifDiv = $("<div class='item'>");
 
